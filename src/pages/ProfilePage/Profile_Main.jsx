25e5c89ba03c5f9bca5cd10a453f6ddaa730c048
kuuku123 <dohyun0047@gmail.com>
2024-05-13 12:50:11 +0900

    done: 1) profile view upgrade 2) now can get profile from spring api
    todo:
    1) spring need to handle updated profile

--- src/pages/ProfilePage/Profile_Main.jsx
+++ src/pages/ProfilePage/Profile_Main.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { CgCalendar, CgMail } from "react-icons/cg";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -11,22 +12,47 @@ const Profile_Main = () => {
     justify-items: center;
     align-items: center;
   `;
-
-  const Profile_Pic_style = styled.div`
-    grid-row-start: 3;
-    grid-row-end: 5;
-    grid-column-start: 1;
-    grid-column-end: 2;
-  `;
-  const Profile_MyProfile_style = styled.div`
+  const Profile_List_style = styled.div`
+    display: flex;
     grid-row-start: 5;
     grid-row-end: 6;
     grid-column-start: 1;
     grid-column-end: 2;
+    flex-direction: column;
+    gap: 2px;
+    margin-right: 13px;
   `;
-  const Profile_Study_style = styled.div`
-    grid-row-start: 6;
-    grid-row-end: 7;
+
+  const Profile_List_Element_style = styled.button`
+    background-color: green;
+    color: white;
+    padding: 0.375rem 0.75rem;
+    width: 300px;
+    font-size: 30px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+
+    /* Hover effect */
+    &:hover {
+      background-color: #0056b3;
+    }
+
+    /* Active effect */
+    &:active {
+      background-color: #004080;
+    }
+
+    /* Disabled styles */
+    &:disabled {
+      background-color: #b3b3b3;
+      color: #666666;
+      cursor: not-allowed;
+    }
+  `;
+  const Profile_Pic_style = styled.div`
+    grid-row-start: 3;
+    grid-row-end: 5;
     grid-column-start: 1;
     grid-column-end: 2;
   `;
@@ -35,33 +61,39 @@ const Profile_Main = () => {
     grid-row-end: 5;
     grid-column-start: 2;
     grid-column-end: 4;
+    font-size: 30px;
   `;
   const Profile_Info_style = styled.div`
     grid-row-start: 4;
     grid-row-end: 5;
     grid-column-start: 2;
     grid-column-end: 4;
+    font-size: 20px;
   `;
   const Profile_Email_style = styled.div`
     grid-row-start: 5;
     grid-row-end: 6;
     grid-column-start: 2;
     grid-column-end: 4;
+    display: flex;
+    justify-content: center;
+    font-size: 30px;
   `;
   const Profile_Emailverfication_style = styled.div`
     grid-row-start: 6;
     grid-row-end: 7;
     grid-column-start: 2;
     grid-column-end: 4;
-  `;
-  const Profile_Edit_style = styled.div`
-    grid-row-start: 7;
-    grid-row-end: 8;
-    grid-column-start: 2;
-    grid-column-end: 4;
+    display: flex;
+    justify-content: center;
+    font-size: 30px;
   `;
 
   const [img, setImage] = useState();
+  const [profile, setProfile] = useState({
+    bio: "default bio",
+    email: "default email",
+  });
 
   useEffect(() => {
     async function fetchProfileImage() {
@@ -81,18 +113,37 @@ const Profile_Main = () => {
     fetchProfileImage();
   }, []);
 
+  useEffect(() => {
+    async function fetchProfile() {
+      const raw_profile = await fetch(`http://localhost:8081/profile`, {
+        credentials: "include",
+        method: "GET",
+      });
+      const profile = await raw_profile.json();
+      console.log("profile.data.email => ", profile.data.email);
+      setProfile(profile.data);
+    }
+    fetchProfile();
+  }, []);
+
   return (
     <Grid_Container_style>
       <Profile_Pic_style>
         <img src={img} width="120px" height="120px"></img>
         <figcaption style={{ textAlign: "center" }}>Profile Image</figcaption>
       </Profile_Pic_style>
-      <Profile_MyProfile_style>myprofile</Profile_MyProfile_style>
-      <Profile_Study_style>study</Profile_Study_style>
+      <Profile_List_style>
+        <Profile_List_Element_style>Profile</Profile_List_Element_style>
+        <Profile_List_Element_style>Study</Profile_List_Element_style>
+      </Profile_List_style>
       <Profile_Name_style>{sessionStorage.getItem("user")}</Profile_Name_style>
-      <Profile_Info_style>info</Profile_Info_style>
-      <Profile_Email_style>email</Profile_Email_style>
+      <Profile_Info_style>{profile.bio || "default bio"}</Profile_Info_style>
+      <Profile_Email_style>
+        <CgMail size={"55px"}></CgMail>
+        {profile.email}
+      </Profile_Email_style>
       <Profile_Emailverfication_style>
+        <CgCalendar size={"55px"}></CgCalendar>
         <Link to={"/profile-setting"}>email verification</Link>
       </Profile_Emailverfication_style>
     </Grid_Container_style>
