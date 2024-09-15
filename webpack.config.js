d59dfb9b1d99444acb2f9266c15ff78fa7d0ac3b
kuuku123 <dohyun0047@gmail.com>
2024-09-15 14:20:32 +0900

    done: 1) build and deployed static content to spring
    todo:
    1) need to figure out automatic way to build and copy to spring

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -42,6 +44,12 @@ module.exports = {
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({
       template: "public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public/images", to: "images" }, // Copy 'public/images' to 'dist/images'
+      ],
     }),
   ],
   devServer: {
