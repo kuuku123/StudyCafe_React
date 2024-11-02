9c5c50959d691f6c9fbbf193da31713a6cb3a8f4
kuuku123 <dohyun0047@gmail.com>
2024-11-02 16:52:15 +0900

    need to check new password twice in social login without existing account

--- src/pages/LoginPage/social/SocialAccountSetPassword/SocialAccountSetPassword_Main.jsx
+++ src/pages/LoginPage/social/SocialAccountSetPassword/SocialAccountSetPassword_Main.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import * as MyForm from "../../../../lib/MyForm";
+import FormControl from "../../../../components/FomrControl";
 
-const SocialAccountSetPassword = () => {
+const SocialAccountSetPassword_Main = () => {
+  const onSubmit = async (passwordInfo) => {
+    const response = await fetch(`${SERVER_API_URL}/settings/password`, {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(passwordInfo),
+    }
+  ).then((res) => {
+      console.log(res);
+      return res.json();
+    });
+    handleResopnse(response)
+  };
+
   return (
-    <div>SocialAccountSetPassword</div>
-  )
-}
+    <MyForm.Form
+      id="social-set-password"
+      initialValue={{
+        password: "",
+      }}
+      onSubmit={onSubmit}
+    >
+      <FormControl
+        label="Password"
+        htmlFor="password"
+        error={<MyForm.ErrorMessage name="password"></MyForm.ErrorMessage>}
+      >
+        <MyForm.Field
+          id="password"
+          name="password"
+          type="password"
+          placeholder="write your password"
+        ></MyForm.Field>
+      </FormControl>
+    </MyForm.Form>
+  );
+};
 
-export default SocialAccountSetPassword
+export default SocialAccountSetPassword_Main;
--- src/pages/LoginPage/social/SocialAccountSetPassword/index.jsx
+++ src/pages/LoginPage/social/SocialAccountSetPassword/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import SocialAccountSetPassword_Main from "./SocialAccountSetPassword_Main";
+import Page from "../../../../components/Page";
+import Title from "../../../../components/Title";
+import * as S from "./SocialAccountSetPassword_Main_style";
+import CopyRight from "../../../../components/CopyRight";
 
 const SocialAccountSetPasswordPage = () => {
-  return <div>SoicalAccountSetPasswordPage</div>;
+  return (
+    <>
+      <Page
+        header={
+          <Title>
+            <S.Header_Input_style></S.Header_Input_style>
+          </Title>
+        }
+        footer={<CopyRight></CopyRight>}
+      >
+        <SocialAccountSetPassword_Main></SocialAccountSetPassword_Main>
+      </Page>
+    </>
+  );
 };
 
 export default SocialAccountSetPasswordPage;
